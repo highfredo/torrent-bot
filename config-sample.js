382070109495fe51c08bf25252a20bc896ce66f4
Highfredo <highfredo@gmail.com>
2016-10-22 17:09:33 +0200

    first commit

---
+++ config-sample.js
@@ -0,0 +1,16 @@
+/*******
+ rename to config.js
+ *******/
+
+module.exports =
+{
+    transmission: {
+        user: "<transmission user>",
+        pass: "<transmission pass>",
+        rpc: "http://<transmission host>:<transmission port>/transmission/rpc"
+    },
+    telegram: {
+        token: '<telegram token>',
+        userId: 0 // <telegram userId>
+    }
+}
---
+++ index.js
@@ -0,0 +1,45 @@
+var config = require('./config')
+var telegram = require('./lib/gateways/telegram')(config.telegram)
+var seedbox = require('./lib/seedboxs/transmission')(config.transmission)
+var tracker = require('./lib/trackers/elitetorrent')
+var types = require('./lib/types')
+var _ = require('lodash')
+
+sendNewTorrents()
+setInterval(sendNewTorrents, 3600000) // 1h 3600000
+
+var lastUpdate;
+function sendNewTorrents() {
+  console.log("BUSCANDO NUEVOS TORRENTS")
+  tracker
+      .latest()
+      .then((result)=>{
+        result.forEach((info) => {
+          if(filter(info)) {
+            console.log("Enviando: " + info.title)
+            telegram.sendTorrentInfo(info)
+          } else {
+            console.log(info.title + ' no ha pasado el filtro')
+          }
+        })
+
+        lastUpdate = new Date()
+      })
+}
+
+function filter(info) {
+  // if(types.SHOW === info.type)
+  //   return false
+
+  if(lastUpdate && info.date < lastUpdate)
+    return false
+
+  return true
+}
+
+telegram.onRequestAddTorrent = function(msg) {
+  tracker.decodeMagnet(msg.data).then((magnet)=>{
+    console.log("Added: " + msg.data)
+    seedbox.addMagnet(magnet)
+  })
+}
